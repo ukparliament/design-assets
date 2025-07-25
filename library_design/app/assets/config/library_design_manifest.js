b491ad0705800b62a11c8b32d24b4991fbcfc798
James Jefferies <james@shedcode.co.uk>
2025-07-25 17:51:42 +0100

    Name FA properly

--- library_design/app/assets/config/library_design_manifest.js
+++ library_design/app/assets/config/library_design_manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../fonts
 //= link bootstrap-4.6.2.css
-//= link fontawesome-5.15.0.css
+//= link fontawesome-5.15.4.css
 //= link pds-design-system.css
 //= link library-extras-1.0.0.css
